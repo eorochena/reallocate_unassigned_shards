fba903fb8d075314a43a9c69f16a076499a0d271
Eduardo Orochena <mint.alchemist@gmail.com>
2017-06-29 14:33:01 -0400

    working on json file

--- bin/reallocate.py
+++ bin/reallocate.py
@@ -4,36 +4,58 @@ import random
 import requests
 import json
 
-elasticsearch_cluster = ['192.168.1.1', '192.168.1.2', '192.168.1.3', '192.168.1.4']
 elasticsearch_port = '9200'
+elasticsearch_node = raw_input('Enter ip of one node that is part of the elaticsearch cluster > ')
+
+
+def elasticsearch_cluster():
+    cluster = []
+    get_cluster = 'http://%s:%s/_cat/nodes?v\&h=ip,r' % (elasticsearch_node, elasticsearch_port)
+    response = requests.get(get_cluster)
+
+    for data_server in response.content.split('\n'):
+        if data_server:
+            if data_server.split()[1] == 'd':
+                cluster.append(data_server.split()[0])
+    return cluster
+
 
 def get_unassigned():
     elasticsearch_url = '%s:%s/_cat/shards?h=index,shard,prirep,state,unassigned.reason' \
-                        % (random.choice(elasticsearch_cluster), elasticsearch_port)
+                        % (random.choice(elasticsearch_cluster()), elasticsearch_port)
     response = requests.get('http://%s' % elasticsearch_url)
 
     full_response = []
+    duplicates = []
     for result in response.content.split('\n'):
-        if 'UNASSIGNED' in result:
-            full_response.append(result)
+        if result:
+            index_shard = result.split()[0] + ' ' + result.split()[1]
+            if 'STARTED' in result and index_shard not in duplicates:
+                duplicates.append(index_shard)
+                full_response.append(result)
     return full_response
+print(len(get_unassigned()))
 
 def reroute():
+    x = 0
     for values in get_unassigned():
         if values:
             payload = {"commands":
                        [{"allocate":
                              {"index":values.split()[0],
                               "shard":values.split()[1],
-                              "node":random.choice(elasticsearch_cluster),
+                              "node":random.choice(elasticsearch_cluster()),
                               "allow_primary":1
                               }
                          }
                         ]
                    }
-            elasticsearch_reroute = '%s:%s/_cluster/reroute' % (random.choice(elasticsearch_cluster),
-                                                                elasticsearch_port)
-            post_request = requests.post(elasticsearch_reroute, data=json.dumps(payload))
-    return post_request
-print(reroute())
+            #elasticsearch_reroute = '%s:%s/_cluster/reroute' % (random.choice(elasticsearch_cluster()),
+             #                                                   elasticsearch_port)
+            #post_request = requests.post(elasticsearch_reroute, data=json.dumps(payload))
+            print(payload)
+            x += 1
+            print(x)
+
+reroute()
 
